4c0f7515bc08b66fee372503b90e56ecc1180146
Fabian Zelaya <fabianzelaya@icloud.com>
2022-07-08 23:53:42 -0700

    new file:   Tester/Tester.java

---
+++ Tester/Tester.java
@@ -0,0 +1,15 @@
+package Tester;
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        // int age;
+
+        System.out.println("Hello, how old are you?");
+
+        Scanner input = new Scanner(System.in);
+
+        int age = input.nextInt();
+    }
+}
