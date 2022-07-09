fa1b9ef0305b910cdbb839abbef635dc6ab47705
Fabian Zelaya <fabianzelaya@icloud.com>
2022-07-09 00:46:19 -0700

    Update Tester.java

--- Tester/Tester.java
+++ Tester/Tester.java
@@ -4,12 +4,28 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        // int age;
 
-        System.out.println("Hello, how old are you?");
+        Scanner in = new Scanner(System.in); // in = input
 
-        Scanner input = new Scanner(System.in);
+        // System.out.print("Hello, how old are you?: ");
+        // int age = in.nextInt();
 
-        int age = input.nextInt();
+        // System.out.print("Please enter the unit price: ");
+        // double unitPrice = in.nextInt();
+
+        System.out.print("Please enter the number of cans");
+        int cans = in.nextInt();
+
+        /**
+         * System.out.print("Please enter the number of cans: ");
+         * 
+         * Console window:
+         * Please enter the number of cans
+         */
     }
 }
+/**
+ * Sample run - Terminal:
+ * Please enter the unit price: 23.5
+ * Exception in thread "main" java.util.InputMismatchException
+ */
