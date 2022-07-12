aa744c359253f210c6e6541449fdeb0d5ca9f39f
Fabian Zelaya <fabianzelaya@icloud.com>
2022-07-12 01:43:06 -0700

    Update if Statement

---
+++ Tester/IfStatement.java
@@ -0,0 +1,24 @@
+package Tester;
+
+/**
+ * @author Fabian Zelaya
+ *         Date: Tue July 12, 2022
+ */
+public class IfStatement {
+    public static void main(String[] args) {
+        int n = 8;
+
+        if (n < 20) {
+            System.out.println("Hi, everyone!");
+        } else {
+            System.out.println("Bye...");
+        }
+    }
+}
+// FZ
+
+/**
+ * Example of the executed code:
+ * 
+ * Hi, everyone!
+ */
