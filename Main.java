32ae242433e2c57b1d99f703810c3264867282f4
Fabian Zelaya <fabianzelaya@icloud.com>
2022-07-17 23:29:24 -0700

    Update

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        final int PERIODS = 5;
-        for (int i = 1; i <= PERIODS; i++) {
-            invest.waitYears(YEARS);
-            System.out.printf(
-                    "The balance after %d years is %3.2f\n",
-                    invest.getYears(), invest.getBalance());
-        }
+        int x = 0;
+        int previous;
+        do {
+            previous = x;
+            x = in.nextInt();
+            sum = sum + x;
+        } while (x != 0 && previous != x);
     }
 }
