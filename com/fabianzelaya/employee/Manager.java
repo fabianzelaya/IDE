8a6db474accb116c00a2e5c77e8ce0ca8ac997cd
Fabian Zelaya <fabianzelaya@icloud.com>
2022-07-29 22:05:34 -0700

    Update

--- com/fabianzelaya/employee/Manager.java
+++ com/fabianzelaya/employee/Manager.java
@@ -15,21 +15,33 @@ public class Manager extends Employee {
     private String name;
 
     // Omit constructors and methods.
-    // public Manager(String name, double baseSalary, double salaryBonus) {
-    // super(name, baseSalary);
-    // this.salaryBonus = salaryBonus;
-    // }
+    public Manager(String name, double baseSalary, double salaryBonus) {
+        super(name, baseSalary);
+        this.salaryBonus = salaryBonus;
+    }
 
-    // public void setSalaryBonus(double salaryBonus) {
-    // this.salaryBonus = salaryBonus;
-    // }
+    public void setSalaryBonus(double salaryBonus) {
+        this.salaryBonus = salaryBonus;
+    }
 
-    // public double getSalaryBonus() {
-    // return salaryBonus;
-    // }
+    public double getSalaryBonus() {
+        return salaryBonus;
+    }
 
-    // public double getSalary() {
-    // return super.getSalary() + salaryBonus;
-    // }
+    public double getSalary() {
+        // return super.getSalary() + salaryBonus;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBaseSalary(double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
 }
 // FZ
