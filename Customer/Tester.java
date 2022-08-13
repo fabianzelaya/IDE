13bdd18cc5a41e054704727d57950e059c063dda
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-08-13 01:57:09 +0000

    Update

--- Customer/Tester.java
+++ Customer/Tester.java
@@ -5,5 +5,18 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args){
         Scanner reader = new Scanner(System.in);
+
+        String name = reader.nextLine();
+        String SecondName = reader.nextLine();
+        int age = reader.nextLine();
+        int roomNumber = reader.nextLine();
+
+        Customer customer = new Customer();
+        customer.name = name;
+        customer.SecondName = SecondName;
+        customer.age = age;
+        customer.roomNumber = roomNumber;
+
+        customer.saveCustomerInfo();
     }
 }
