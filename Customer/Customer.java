e5d6905b893fa5c90b9bb94d78626b0e2828b1a5
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-08-13 01:30:33 +0000

    Update

---
+++ Customer/Customer.java
@@ -0,0 +1,24 @@
+package Customer;
+
+public class Customer {
+    private String name;
+    private String SecondName;
+    private int age;
+    private int roomNumber;
+
+    // public Customer(String name, String SecondName, int age, int roomNumber) {
+    // this.name = name;
+    // this.SecondName = SecondName;
+    // this.age = age;
+    // this.roomNumber = roomNumber;
+    // }
+
+    public void saveCustomerInfo() {
+        System.out.println("Customer info saved");
+        System.out.println("First name: " + name);
+        System.out.println("Second name: " + SecondName);
+        System.out.println("Age: " + age);
+        System.out.println("Room name: " + roomNumber);
+    }
+}
+// FZ
