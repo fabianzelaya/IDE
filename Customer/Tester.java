60739de30befcbd0f52d136c766bf5cb31a268cf
Fabian <71942518+fabianzelaya@users.noreply.github.com>
2022-08-13 01:34:32 +0000

    Update

--- Customer/Tester.java
+++ Customer/Tester.java
@@ -1,5 +1,9 @@
 package Customer;
 
+import java.util.Scanner;
+
 public class Tester {
-    
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+    }
 }
